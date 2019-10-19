0ec0603f9b25e108f232c90ed839c95d5136233e
JALAKAM Rajiv <rajiv.jalakam@hexagonsi.com>
2019-10-19 15:34:21 +0530

    First chart

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { chart } from 'chart.js';
+import { Component, ViewChild } from '@angular/core';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +7,40 @@ import { chart } from 'chart.js';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  @ViewChild('barChart') barChart;
+
+  bars: any;
+  colorArray: any;
 
   constructor() {}
+
+  ionViewDidEnter() {
+    this.createBarChart();
+  }
+
+  createBarChart() {
+    this.bars = new Chart(this.barChart.nativeElement, {
+      type: 'bar',
+      data: {
+        labels: ['S1', 'S2', 'S3', 'S4', 'S5', 'S6', 'S7', 'S8'],
+        datasets: [{
+          label: 'Viewers in millions',
+          data: [2.5, 3.8, 5, 6.9, 6.9, 7.5, 10, 17],
+          backgroundColor: 'rgb(38, 194, 129)', // array should have same number of elements as number of dataset
+          borderColor: 'rgb(38, 194, 129)',// array should have same number of elements as number of dataset
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
+            }
+          }]
+        }
+      }
+    });
+  }
 
 }
